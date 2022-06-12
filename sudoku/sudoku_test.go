98f610b3e23049b76c9fb004e0fbb7c0573a0603
Craymaru <craymaru@gmail.com>
2022-06-12 20:25:19 +0900

    test: Sudoku

---
+++ sudoku/sudoku_test.go
@@ -0,0 +1,117 @@
+package sudoku
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestDuplicated(t *testing.T) {
+	if Duplicated([10]int{
+		0, 0, 0, 0, 0, 0, 0, 0, 0,
+	}) {
+		t.Fatal("Not duplicated nit failed.")
+	}
+
+	if !Duplicated([10]int{
+		0, 2, 0, 0, 0, 0, 0, 0, 0,
+	}) {
+		t.Fatal("It's duplicated.")
+	}
+}
+
+func TestVerify(t *testing.T) {
+	cases := []struct {
+		message  string
+		board    Board
+		expected bool
+	}{
+		{
+			message: "All zero",
+			board: Board{
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			},
+			expected: true,
+		},
+		{
+			message: "Row duplicated",
+			board: Board{
+				{1, 0, 1, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			},
+			expected: false,
+		},
+		{
+			message: "Column duplicated",
+			board: Board{
+				{1, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{1, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			},
+			expected: false,
+		},
+		{
+			message: "Square duplicated",
+			board: Board{
+				{1, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 1, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+				{0, 0, 0, 0, 0, 0, 0, 0, 0},
+			},
+			expected: false,
+		},
+	}
+
+	for num, val := range cases {
+		t.Run(fmt.Sprintf("#%d %v", num, val.message), func(t *testing.T) {
+			got := Verify(val.board)
+			if got != val.expected {
+				t.Errorf("Want %v, Got %v.", val.expected, got)
+			}
+		})
+	}
+}
+
+func TestSolve(t *testing.T) {
+	board := Board{
+		{0, 5, 0, 0, 8, 3, 0, 1, 7},
+		{0, 0, 0, 1, 0, 0, 4, 0, 0},
+		{3, 0, 4, 0, 0, 5, 6, 0, 8},
+		{0, 0, 0, 0, 3, 0, 0, 0, 9},
+		{0, 9, 0, 8, 2, 4, 5, 0, 0},
+		{0, 0, 6, 0, 0, 0, 0, 7, 0},
+		{0, 0, 9, 0, 0, 0, 0, 5, 0},
+		{0, 0, 7, 2, 9, 0, 0, 8, 6},
+		{1, 0, 3, 6, 0, 7, 2, 0, 4},
+	}
+
+	if !Backtrack(&board) {
+		t.Fatal(("Should be solved, but wasn't."))
+	}
+	fmt.Printf("Resolved!\n%+v\n", Pretty(board))
+}
